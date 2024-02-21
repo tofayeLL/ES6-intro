666bf6545993f96c3d03975fa8007f2e23ca5153
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-21 22:28:27 +0600

    learn and practice about arrow function basic how to declare

---
+++ arrow.js
@@ -0,0 +1,29 @@
+
+// function
+/* function add (num1, num2){
+    return num1+num2;
+}
+const sum = add(50, 40);
+console.log(sum); */
+
+
+
+/* -----function expression------ */
+/* const add2 = function(num1, num2){
+    return num1+num2;
+}
+const sum = add2(50, 40);
+console.log(sum); */
+
+
+ /* ----arrow function----- */
+ const add3 = (num1, num2) => num1 + num2;
+ const sum = add3(50, 40);
+//  console.log(sum);
+
+
+ const add4 = (num1, num2, num3, num4) => num1 + num2 + num3 * num4;
+ const total = add4 (3, 4, 5, 7);
+ console.log(total);
+
+
