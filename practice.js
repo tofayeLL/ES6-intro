cb293f3413d10092b8a56bde5cb0259b2c802be5
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-22 16:43:09 +0600

    finished practice problem 3

--- practice.js
+++ practice.js
@@ -21,17 +21,44 @@
 
 
 /* --------practice problem 2---------- */
-const friends = (names) => {
-    let evenNames = [];
-    for (let name of names) {
-        if (name.length % 2 === 0) {
-            evenNames.push(name);
+// print even friends name in a new array use array function//
+// const friends = (names) => {
+//     let evenNames = [];
+//     for (let name of names) {
+//         if (name.length % 2 === 0) {
+//             evenNames.push(name);
 
-        }
+//         }
+//     }
+//     return evenNames;
+// }
+
+// const names = ['akash', 'batash', 'sagor', 'nodii', 'pani', 'prithibi', 'chad', 'tara', 'surgo'];
+// const evenNames = friends(names);
+// console.log(evenNames);
+
+
+
+
+/* ----------------------------------------------------------- */
+
+/* ------practice problem 3-------- */
+// average of numbers in the array
+const averageNumbers = (numbers) => {
+    let sum = 0;
+    let squareNum = [];
+    for (let num of numbers) {
+        let square = Math.pow(num, 2);
+        sum = sum + square;
+        squareNum.push(square);   
     }
-    return evenNames;
+    // console.log(squareNum);
+    // console.log(sum)
+    const length = squareNum.length;
+    const average = sum / length;
+    return average;
+    
 }
 
-const names = ['akash', 'batash', 'sagor', 'nodii', 'pani', 'prithibi', 'chad', 'tara', 'surgo'];
-const evenNames = friends(names);
-console.log(evenNames);
+const avg = averageNumbers([2, 3, 4, 5, 6]);
+console.log(avg);
