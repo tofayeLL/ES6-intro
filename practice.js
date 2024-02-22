a599e3de1cad69ababce3aca81e43ad3ef859bd5
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-22 16:07:10 +0600

    finished practice problem 1

---
+++ practice.js
@@ -0,0 +1,17 @@
+/* -------practice problem 1----- */
+// number 1
+const multiply = (x, y, z) => x * y * z;
+// const result = multiply(2, 3, 4);
+// console.log(result);
+
+// number 2
+const sentence = 'I am a web developer. I love to code. I love to eat biriyani';
+const convert = `I am a web developer.
+I love to code.
+I love to eat biriyani.`;
+console.log(convert);
+
+// number 3
+const add = (x, y = 2) => x + y;
+const result = add(4);
+console.log(result);
